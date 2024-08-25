2623020e72764e29bcda532c4c1e4c83631ede27
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2024-08-25 03:15:08 -0300

    Adiciona protocolo para Endpoint, HTTPBody e ViewCode

--- Package.swift
+++ Package.swift
@@ -8,16 +8,15 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Protocols",
-            targets: ["Protocols"]),
+            name: "Network",
+            targets: ["Network"]),
+        .library(name: "ViewCode",
+                 targets: ["ViewCode"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Protocols"),
-        .testTarget(
-            name: "ProtocolsTests",
-            dependencies: ["Protocols"]),
+        .target(name: "Network"),
+        .target(name: "ViewCode")
     ]
 )
